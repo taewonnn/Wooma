d6ea1227126d330aacbfba4335b131b62eb1d149
taewon <eden@rainbow.co.kr>
2024-03-07 12:25:56 +0900

    feat: 회원가입 validation

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,13 +2,25 @@ import { useForm } from 'react-hook-form';
 import { IForm } from '../types/sign';
 
 function Signup() {
-  /** hook-form */
-  const { register, watch, handleSubmit, setError } = useForm<IForm>();
+  /** useForm */
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>();
   console.log('Input에 입력한 값 확인 :', watch());
   console.log('check: ', setError);
 
   /** validation이 끝난 뒤 실행 함수 */
-  const onValid = () => {};
+  const onValid = (data: IForm) => {
+    console.log('제출한 데이터 묶음 확인:', data);
+    if (data.password !== data.passwordConfirm) {
+      setError('passwordConfirm', { message: 'password are not the same' }, { shouldFocus: true });
+    }
+  };
+
   return (
     <>
       <p>Signup Page</p>
@@ -19,8 +31,8 @@ function Signup() {
             {...register('email', {
               required: 'Please Write Email',
               pattern: {
-                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                message: 'Only naver.com emails allowed',
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Please write valid email format',
               },
             })}
             type="text"
@@ -29,23 +41,32 @@ function Signup() {
             id="email"
           />
         </div>
+        <span className="text-red-500">{errors?.email?.message}</span>
 
         <div className="flex justify-start mb-4">
           <label htmlFor="username">username :</label>
           <input
-            {...register('username', { required: 'Please Write username' })}
+            {...register('username', {
+              required: 'Please Write username',
+              pattern: {
+                value: /^[a-zA-Z]{3,20}$/,
+                message:
+                  'Username must be 3-20 characters long and can only include upper and lower case letters',
+              },
+            })}
             type="text"
             placeholder="username"
             name="username"
             id="username"
           />
         </div>
+        <span className="text-red-500">{errors?.username?.message}</span>
 
         <div className="flex justify-start mb-4">
           <label htmlFor="password">password :</label>
           <input
             {...register('password', { required: 'Please Write password' })}
-            type="text"
+            type="password"
             placeholder="password"
             name="password"
             id="password"
@@ -56,7 +77,7 @@ function Signup() {
           <label htmlFor="passwordConfirm">passwordConfirm :</label>
           <input
             {...register('passwordConfirm', { required: 'Please Write passwordConfirm' })}
-            type="text"
+            type="password"
             placeholder="passwordConfirm"
             name="passwordConfirm"
             id="passwordConfirm"
