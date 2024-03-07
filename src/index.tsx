3b3b538e519a0b2f46e7bf8f0d010f84af6a9caa
taewon <eden@rainbow.co.kr>
2024-03-07 10:20:13 +0900

    feat: router 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { authenticatedUserRouter, unauthenticatedUserRouter } from './router/router';
+
+/** 로그인 상태 확인 함수 */
+const isLoggedIn = () => {
+  // 테스트 위해 user 지정
+  const user = 1;
+  if (user) {
+    return true;
+  }
+  return false;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={isLoggedIn() ? authenticatedUserRouter : unauthenticatedUserRouter} />
   </React.StrictMode>
 );
