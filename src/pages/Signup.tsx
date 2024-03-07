2154f57e8320c37c8d158b3d42fa055569f1000f
taewon <eden@rainbow.co.kr>
2024-03-07 15:24:54 +0900

    feat: 회원가입 validation

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -31,31 +31,13 @@ function Signup() {
       <p>Signup Page</p>
       <form onSubmit={handleSubmit(onValid)} className="flex flex-col ">
         <div className="flex justify-start mt-4 mb-4">
-          <label htmlFor="email">email :</label>
+          <label htmlFor="username">username :</label>
           <input
-            {...register('email', {
+            {...register('username', {
               required: '필수 : 이메일',
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                 message: '이메일 형식에 맞춰 입력해주세요',
-              },
-            })}
-            type="text"
-            placeholder="email"
-            name="email"
-            id="email"
-          />
-        </div>
-        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-
-        <div className="flex justify-start mb-4">
-          <label htmlFor="username">username :</label>
-          <input
-            {...register('username', {
-              required: '필수 : 이름',
-              pattern: {
-                value: /^[a-zA-Z가-힣]{3,20}$/,
-                message: '3 ~ 10 글자 이내로 입력해주세요',
               },
             })}
             type="text"
@@ -65,6 +47,24 @@ function Signup() {
           />
         </div>
         {errors.username && <p className="text-red-500">{errors.username.message}</p>}
+
+        <div className="flex justify-start mb-4">
+          <label htmlFor="name">name :</label>
+          <input
+            {...register('name', {
+              required: '필수 : 이름',
+              pattern: {
+                value: /^[a-zA-Z가-힣]{3,20}$/,
+                message: '3 ~ 10 글자 이내로 입력해주세요',
+              },
+            })}
+            type="text"
+            placeholder="name"
+            name="name"
+            id="name"
+          />
+        </div>
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
 
         <div className="flex justify-start mb-4">
           <label htmlFor="password">password :</label>
