3e6e7c497a4dec782c1955ef170384fe49c28c88
taewonnn <thisis_tw94@naver.com>
2024-03-08 09:41:41 +0900

    feat: 캘린더 적용

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,8 +1,17 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { useState } from 'react';
 
 function Calendar() {
-  /** 이벤트 관리 */
+  /** 일자 상태 */
+  const [date, SetDate] = useState({});
+
+  /** 일자 클릭 시 이벤트 */
+  const handleDateClick = (arg: any) => {
+    console.log(arg);
+    //console.log(arg.event._def);
+    SetDate('arg');
+  };
 
   return (
     <>
@@ -11,6 +20,7 @@ function Calendar() {
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
+          eventClick={handleDateClick}
           events={[
             { title: '테스트1', date: '2024-03-20' },
             { title: '테스트2', date: '2024-03-15' },
