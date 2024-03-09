6f84df460602b0b6744973c4a67161bbe5c59dd7
TaeWon Park <thisis_tw94@naver.com>
2024-03-09 12:18:11 +0900

    feat: create 초안

--- src/components/calendar/Create.tsx
+++ src/components/calendar/Create.tsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
+
 function Create({ selectedDate }: any) {
+  const [transactionType, setTransactionType] = useState('');
+
+  const handleTransactionTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setTransactionType(event.target.value);
+  };
+
   return (
     <>
       create
-      <label></label>
+      <label htmlFor="category">유형: </label>
       <p>{selectedDate}</p>
-      <input type="text" name="" id="" />
+      <select
+        name="transactionType"
+        id="category"
+        value={transactionType}
+        onChange={handleTransactionTypeChange}
+      >
+        <option value="">--Please choose an option--</option>
+        <option value="expenditure">지출</option>
+        <option value="deposit">수입</option>
+      </select>
+      <label htmlFor="amount">금액:</label>
+      <input type="text" name="amount" id="amount" />
+      <button>입력</button>
+      <button>취소</button>
     </>
   );
 }
