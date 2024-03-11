f85e92cf2ebeeb0c1fbe1b3cfd8f3f4c4aeb2704
taewonnn <thisis_tw94@naver.com>
2024-03-11 18:13:48 +0900

    feat: json server용 / 개발용

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,31 @@
 import { ICreateForm } from './types/calendar';
 
 /** 입력한 기존 수입/지출내역 가져오기 - 테스트용 json */
+/** json -server X : /testdata.json */
 export function getfinancialTransactions() {
-  return fetch('http://localhost:3000/transactions')
+  return fetch('/testdata.json')
     .then((res) => {
       if (!res.ok) {
         throw new Error('Network error');
       }
       return res.json();
     })
-    .then((data) => data);
+    .then((data) => data.transactions);
 }
+
+/** 입력한 기존 수입/지출내역 가져오기 - 테스트용 json */
+/** json -server 이용시 : http://localhost:3000/transactions */
+// $ : json-server --watch public/testdata.json
+// export function getfinancialTransactions() {
+//   return fetch('http://localhost:3000/transactions')
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw new Error('Network error');
+//       }
+//       return res.json();
+//     })
+//     .then((data) => data);
+// }
 
 /** 내역 추가하기 - 테스트용 json */
 export function postFinancialTransactions(newData: ICreateForm) {
