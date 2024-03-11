430fdb6863865bc9a07dc939bfb38629412b53b6
taewonnn <thisis_tw94@naver.com>
2024-03-11 15:36:18 +0900

    feat: api get / post

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,22 @@
 /** 입력한 기존 수입/지출내역 가져오기 - 테스트용 json */
 export function getfinancialTransactions() {
-  return fetch('/testdata.json').then((res) => {
+  return fetch('/testdata.json')
+    .then((res) => res.json())
+    .then((data) => data.transactions);
+}
+
+/** 내역 추가하기 - 테스트용 json */
+export function postFinancialTransactions(newData: any) {
+  return fetch('/testdata.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newData),
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error('newtwork erro');
+    }
     return res.json();
   });
 }
