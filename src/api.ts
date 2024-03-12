8460a1f7fc2d9dae4d4c0be1de78fca2c06a9f24
taewonnn <thisis_tw94@naver.com>
2024-03-12 11:26:16 +0900

    feat: test json 변경

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,8 @@ import { ICreateForm } from './types/calendar';
 
 /** 입력한 기존 수입/지출내역 가져오기 - 테스트용 json */
 /** json -server X : /testdata.json */
-export function getfinancialTransactions() {
-  return fetch('/testdata.json')
+export function getTransactions() {
+  return fetch('/testdata2.json')
     .then((res) => {
       if (!res.ok) {
         throw new Error('Network error');
