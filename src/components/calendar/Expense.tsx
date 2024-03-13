9b1bc312bcc4ed5b97aa4b81f1c2da441da45e96
taewonnn <thisis_tw94@naver.com>
2024-03-13 14:10:57 +0900

    feat: 소비금액 작성

--- src/components/calendar/Expense.tsx
+++ src/components/calendar/Expense.tsx
@@ -2,31 +2,36 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { ICreateExpenseForm } from '../../types/calendar';
 import { useForm } from 'react-hook-form';
 import { dateClickedState, selectedDateState } from '../../atoms';
+import { v4 as uuidv4 } from 'uuid';
 
 function Expense() {
-  /** hook-form 입력값 */
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<ICreateExpenseForm>({ mode: 'onChange' });
-  console.log('입력값 확인: ', watch());
-
-  /** validation 끝난 이후 실행함수 */
-  const onValid = (data: ICreateExpenseForm) => {
-    console.log('제출한 데이터 묶음 확인:', data);
-
-    // UUID 적용
-
-    // POST - Mutaion
-  };
-
   /** 선택한 일자 */
   const selectedDate = useRecoilValue(selectedDateState);
 
   /** 일자 클릭 상태 변경 -> 모달 닫기 */
   const setDateClicked = useSetRecoilState(dateClickedState);
+
+  /** hook-form 입력값 */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ICreateExpenseForm>({ mode: 'onChange' });
+  // console.log('입력값 확인: ', watch());
+
+  /** validation 끝난 이후 실행함수 */
+  const onValid = (data: ICreateExpenseForm) => {
+    // console.log('제출한 데이터 묶음 확인:', data);
+
+    // amount -> Number
+    data.amount = Number(data.amount);
+
+    // UUID 적용
+    const formData = { ...data, UUID: uuidv4() };
+    console.log('UUID 포함 데이터: ', formData);
+
+    // POST - Mutaion
+  };
 
   return (
     <>
