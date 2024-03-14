eb27eaec9b255b1e0a9072dbf7bd5840d5ca2bd1
taewonnn <thisis_tw94@naver.com>
2024-03-14 10:13:10 +0900

    feat: diary 구조

--- src/components/calendar/Diary.tsx
+++ src/components/calendar/Diary.tsx
@@ -1,7 +1,27 @@
 function Diary() {
   return (
     <>
-      <p>Diary page</p>
+      <form>
+        <label htmlFor=""></label>
+        <input type="text" />
+
+        <label htmlFor=""></label>
+        <input type="text" />
+
+        <label htmlFor=""></label>
+        <input type="textarea" />
+
+        <label htmlFor=""></label>
+        <div className="display-none">이미지 영역</div>
+
+        <label htmlFor=""></label>
+        <input type="file" />
+
+        <button>입력</button>
+
+        <hr />
+        <button>닫기</button>
+      </form>
     </>
   );
 }
