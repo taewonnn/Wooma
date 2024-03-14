f379534310fa21cf7a2ecacf123c0e32680c8f04
taewonnn <thisis_tw94@naver.com>
2024-03-14 15:58:25 +0900

    style: diary

--- src/components/calendar/Diary.tsx
+++ src/components/calendar/Diary.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function Diary() {
   /** 이미지 노출 상태 */
   const [image, setImage] = useState('');
+  const [imageURL, setImageURL] = useState('');
 
+  /** 이미지 보여주기 함수 */
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const img = event.target.files;
     if (!img) return;
@@ -12,26 +15,40 @@ function Diary() {
     const reader = new FileReader();
 
     reader.onloadend = () => {
-      setImage(reader.result as string);
+      const url = reader.result as string;
+      setImageURL(url);
+      setValue('imageURL', url);
     };
 
     reader.readAsDataURL(file);
   };
 
+  /** useForm */
+  const { register, watch, setValue, handleSubmit } = useForm();
+
+  /** validation 완료 이후 */
+  const onValid = () => {};
+
+  console.log(watch());
   return (
     <div className="pb-20">
-      <form>
+      <form onSubmit={handleSubmit(onValid)}>
         <label htmlFor="title">타이틀 : </label>
-        <input type="text" id="title" />
+        <input {...register('title')} type="text" id="title" />
 
         <label htmlFor="content">내용: </label>
-        <input className="w-full h-64 border border-gray-200" type="textarea" id="content" />
+        <input
+          {...register('content')}
+          className="w-full h-64 border border-gray-200"
+          type="textarea"
+          id="content"
+        />
 
         <div className="w-full h-64 border border-gray-200 flex items-center justify-center">
           {image ? <img src={image} alt="preview" className="max-w-full max-h-full" /> : ''}
         </div>
-        <label htmlFor="fileInput">이미지:</label>
-        <input onChange={handleImageChange} id="fileInput" type="file" />
+        <label htmlFor="imageURL">이미지:</label>
+        <input {...register('imageURL')} onChange={handleImageChange} id="imageURL" type="file" />
 
         <hr />
         <button>닫기</button>
