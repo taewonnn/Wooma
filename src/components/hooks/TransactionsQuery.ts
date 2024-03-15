49770fff8a82ebbf67ca5f7f91c87b2403d24c8c
taewonnn <thisis_tw94@naver.com>
2024-03-15 10:54:30 +0900

    feat: json-server용

--- src/components/hooks/TransactionsQuery.ts
+++ src/components/hooks/TransactionsQuery.ts
@@ -9,3 +9,15 @@ const fetchTransactions = async () => {
 export const useTransactions = () => {
   return useQuery({ queryKey: ['transaction'], queryFn: fetchTransactions });
 };
+
+/** json data 이용 시 */
+// cli : json-server --watch public/expenseData.json
+
+// const fetchTransactionsJsonData = async () => {
+//   const data = await get('http://localhost:3000/transactions');
+//   return data.transaction;
+// };
+
+// export const useTransactions = () => {
+//   return useQuery({ queryKey: ['transaction'], queryFn: fetchTransactionsJsonData });
+// };
