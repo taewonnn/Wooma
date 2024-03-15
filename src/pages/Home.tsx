41ed46838a094666630f919adea12e8aebfd15f9
taewonnn <thisis_tw94@naver.com>
2024-03-15 12:29:11 +0900

    feat: diary 목록 가져오기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useDiary } from '../components/hooks/DiaryQuery';
 
 function Home() {
-  const { isLoading, data } = useDiary;
+  const { isLoading, data } = useDiary();
   return (
     <>
       <p>Home Page</p>
