bf722a92a29d42405a68547da13e94864340d10b
taewonnn <thisis_tw94@naver.com>
2024-03-15 10:33:32 +0900

    chore: 주석 제거

--- src/components/hooks/useTransactions.ts
+++ src/components/hooks/useTransactions.ts
@@ -1,15 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { get } from '../../api';
 
-// const fetchTransactions = async () => {
-//   const response = await fetch('/expenseData.json');
-//   if (!response.ok) {
-//     throw new Error('Network error');
-//   }
-
-//   return response.json().then((data) => data.transactions);
-// };
-
 const fetchTransactions = async () => {
   const data = await get('/expenseData.json');
   return data.transactions;
