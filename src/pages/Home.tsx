77b1bce837ee024c905cfefb669d7b6bcc6af47e
TaeWon Park <thisis_tw94@naver.com>
2024-03-16 21:13:34 +0900

    feat: home - 다이어리 보여주기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
 import { useDiary } from '../components/hooks/DiaryQuery';
 
 function Home() {
-  const { isLoading, data } = useDiary();
-  console.log(data);
+  const { isLoading, data: diaryList } = useDiary();
+  console.log(diaryList);
   return (
     <>
       <p>Home Page</p>
+      <h1>diary</h1>
+      <p>
+        {diaryList &&
+          diaryList.map((diary: any) => (
+            <>
+              <p>{diary.title}</p>
+            </>
+          ))}
+      </p>
     </>
   );
 }
