e82f1cf7495b586ea9f5c1bea3bc22c6ed2ee89f
TaeWon Park <thisis_tw94@naver.com>
2024-03-16 22:17:18 +0900

    chore: 페이지 구조 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ function Footer() {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 flex justify-around p-4 bg-gray-200">
-      <button onClick={onClickPage}>home</button>
+      <button onClick={onClickPage}>feed</button>
       <button onClick={onClickPage}>calendar</button>
       <button onClick={onClickPage}>assets</button>
       <button onClick={onClickPage}>settings</button>
