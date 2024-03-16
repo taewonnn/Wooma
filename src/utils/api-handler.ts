2617cee7bd9d71ccaf05be120c888c1127f7212e
TaeWon Park <thisis_tw94@naver.com>
2024-03-17 00:36:09 +0900

    feat: error handler

--- src/utils/api-handler.ts
+++ src/utils/api-handler.ts
@@ -1,5 +1,10 @@
-/** 성공 시 */
-export function fetchSuccess() {}
-
-/** 실패 시 */
-export function fetchError() {}
+/** 에러 핸들링 함수 */
+export function handleError(response: Response) {
+  if (!response.ok) {
+    if (response.status >= 400 && response.status < 500) {
+      throw new Error(`Client error: ${response.status}`);
+    } else if (response.status >= 500) {
+      throw new Error(`Server error: ${response.status}`);
+    }
+  }
+}
