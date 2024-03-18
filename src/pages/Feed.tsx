e68090c0fea29f9b526be70adad103aebb2d4f0c
taewonnn <thisis_tw94@naver.com>
2024-03-18 11:09:28 +0900

    feat: diary 카드 컴포넌트

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,27 +1,32 @@
+import FeedCard from '../components/feed/FeedCard';
 import { useDiary } from '../hooks/DiaryQuery';
 import { IDiaryData } from '../types/diary';
 
 function Feed() {
   const { isLoading, data: diaryList } = useDiary();
-  console.log('diary data:', diaryList);
+  // console.log('diary data:', diaryList);
 
   return (
-    <div className="pb-20">
-      <p>diary</p>
+    <>
+      <div className="pb-20">
+        <p>diary</p>
 
-      {!isLoading &&
-        diaryList &&
-        diaryList.map((diary: IDiaryData) => (
-          <div key={diary.id}>
-            <h3>{diary.title}</h3>
-            <div>
-              {diary.imageURL && (
-                <img src={diary.imageURL} alt="" className="w-[100px] h-[100px]" />
-              )}
+        <FeedCard diaryList={diaryList} />
+
+        {!isLoading &&
+          diaryList &&
+          diaryList.map((diary: IDiaryData) => (
+            <div key={diary.id}>
+              <h3>{diary.title}</h3>
+              <div>
+                {diary.imageURL && (
+                  <img src={diary.imageURL} alt="" className="w-[100px] h-[100px]" />
+                )}
+              </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 }
 
