447e36098b269a31f58da5bfdbe7872eef6ee4c3
TaeWon Park <thisis_tw94@naver.com>
2024-07-03 21:54:07 +0900

    feat: 지출 / 일기 모달 수정 중

--- src/components/calendar/EntrySwitcher.tsx
+++ src/components/calendar/EntrySwitcher.tsx
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Diary from './Diary';
 import Expense from './Expense';
+import { Tab } from '@headlessui/react';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 function EntrySwitcher({ closeModal }: { closeModal: () => void }) {
+  let [categories] = useState({
+    지출: [
+      {
+        id: 1,
+        title: 'Does drinking coffee make you smarter?',
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: '2h ago',
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    일기: [
+      {
+        id: 1,
+        title: 'Is tech making coffee better or worse?',
+        date: 'Jan 7',
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: 'The most innovative things happening in coffee',
+        date: 'Mar 19',
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+  });
   /** toggle 상태 */
   const [isSelected, SetIsSelected] = useState(false);
 
@@ -22,6 +61,26 @@ function EntrySwitcher({ closeModal }: { closeModal: () => void }) {
           {isSelected ? '일기' : '지출'}
         </div>
       </div>
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+          {Object.keys(categories).map(category => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+                  'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                  selected
+                    ? 'bg-white text-blue-700 shadow'
+                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+      </Tab.Group>
 
       {isSelected ? <Diary closeModal={closeModal} /> : <Expense closeModal={closeModal} />}
     </>
