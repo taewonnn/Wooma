66b78425bce7c69f9901d4edaa7e55fabea724a3
eden <eden@rainbow.co.kr>
2024-12-24 12:28:53 +0900

    style: 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ export default {
   darkMode: false,
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        main: '#7241FB',
+        red: '#FF3844',
+        black: '#222222',
+        white: '#FFFFFF',
+        gray: '#999999',
+        line: '#F8F8F9',
+        naverGreen: '#03C75A',
+        kakaoYellow: '#FFE500',
+      },
     },
   },
   variants: {
