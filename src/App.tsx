0e22b53f4ffac8c30278f00681aaaa68cbb96c76
eden <eden@rainbow.co.kr>
2024-12-24 11:50:02 +0900

    style: mobile웹 기준 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 
-function App() {
+export default function App() {
   return (
-    <>
+    <div className="max-w-[768px] mx-auto p-4 min-h-screen flex flex-col justify-between bg-gray-100">
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
-
-export default App;
--- src/PublicApp.tsx
+++ src/PublicApp.tsx
@@ -2,21 +2,19 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { isLoggedIn } from '.';
 import { useEffect } from 'react';
 
-function PublicApp() {
+export default function PublicApp() {
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!isLoggedIn()) {
-  //     navigate('/signIn');
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      navigate('/signIn');
+    }
+  }, []);
 
   return (
-    <div>
+    <div className="max-w-[768px] mx-auto p-4 min-h-screen flex flex-col justify-between bg-gray-100">
       <p className="bg-red-500">Login Please</p>
       <Outlet />
     </div>
   );
 }
-
-export default PublicApp;
