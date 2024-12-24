1e851888d3c7dc1a4621d820b6358e67a0fef1f1
eden <eden@rainbow.co.kr>
2024-12-24 12:13:21 +0900

    install: svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
     host: true, // localhost 대신 true로 변경
     port: 3002,
