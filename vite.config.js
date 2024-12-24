90e92fb25033f241d7f03b97596c60d11976692d
eden <eden@rainbow.co.kr>
2024-12-24 11:15:18 +0900

    feat: vite 설정 - port / 자동열기

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: true, // localhost 대신 true로 변경
+    port: 3002,
+    open: true, // 브라우저 자동으로 열기
   },
   resolve: {
     alias: {
