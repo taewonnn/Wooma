49782d7023e476039ffb676c674a70872f292e2d
eden <eden@rainbow.co.kr>
2024-12-24 12:27:21 +0900

    feat: 푸터 추가

--- src/PublicApp.tsx
+++ src/PublicApp.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import { isLoggedIn } from '.';
 import { useEffect } from 'react';
+import Footer from './components/footer/Footer';
 
 export default function PublicApp() {
   const navigate = useNavigate();
@@ -13,8 +14,8 @@ export default function PublicApp() {
 
   return (
     <div className="max-w-[768px] mx-auto p-4 min-h-screen flex flex-col justify-between bg-gray-100">
-      <p className="bg-red-500">Login Please</p>
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+export default function Footer() {
+  const handleMapClick = () => {
+    console.log('지도 클릭!');
+    // 카카오맵 열기
+  };
+
+  return (
+    <footer className="flex justify-center bg-white p-4">
+      <div className="w-full max-w-screen-lg text-center text-xs text-999">
+        WOOMA | 대표이사 : 대표이사 | 사업자 등록번호 : XXXX-XXX-XXX-XXXX <br />
+        <span className="cursor-pointer underline" onClick={handleMapClick}>
+          (06167) 서울특별시 강남구
+        </span>
+        <br />
+        유선 : +82-2-111-1111 | 팩스 : +82-2-123-4567 | E-mail :{' '}
+        <Link className="cursor-pointer underline" to="mailto: partners@rainbow.co.kr">
+          woomna@wooma.co.kr
+        </Link>
+        <br />
+        Copyright(c) WOOMA. All rights reserved.
+      </div>
+    </footer>
+  );
+}
