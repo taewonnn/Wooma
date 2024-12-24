56c0b88441aafeca56fbf6e2eba7cabfa3f064b7
eden <eden@rainbow.co.kr>
2024-12-24 11:23:52 +0900

    feat: env config

---
+++ src/constants/envConfig.ts
@@ -0,0 +1,17 @@
+/** dev / prod */
+
+/** naver login */
+export const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
+export const NAVER_REDIRECT_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
+
+export const NAVER_SECRET = import.meta.env.VITE_NAVER_SECRET;
+
+/** kakao login */
+export const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
+export const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+
+/** kakao map */
+export const KAKAO_APP_KEY = import.meta.env.VITE_KAKAO_APP_KEY;
+
+/** Img */
+export const IMG_URL = import.meta.env.VITE_BASE_IMG_URL;
