10fafe0f574d82907f1d3ba601af0cab89b6110d
taewonnn <thisis_tw94@naver.com>
2025-01-02 13:36:39 +0900

    feat: calendar - 지출 내역 모달

--- src/components/member/calendar/MainCalendar.tsx
+++ src/components/member/calendar/MainCalendar.tsx
@@ -3,6 +3,9 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import useCalendarStore from '../../../stores/calendar/useCalendarStore';
+import Button from '../../common/button/Button';
+import { useModalStore } from '../../../stores/useModalStore';
+import ExpenseData from './ExpenseData';
 
 // mockdata
 const events = [
@@ -12,7 +15,19 @@ const events = [
 ];
 
 function MainCalendar() {
+  // 캘린더 선택일자 상태
   const { setSelectedDate } = useCalendarStore();
+
+  // 모달
+  const { openModal } = useModalStore();
+
+  // 모달 함수
+  const modalOpen = () => {
+    openModal('custom', {
+      title: '내역',
+      customContent: <ExpenseData />,
+    });
+  };
 
   // 특정 일자 클릭 시
   const handleDateClick = (info: any) => {
@@ -24,7 +39,7 @@ function MainCalendar() {
     setSelectedDate(info.event.startStr); // 이벤트 날짜를 상태에 저장
   };
 
-  // 캘린더 redner
+  // 캘린더 일자 event redner
   const renderEventContent = (eventInfo: any) => {
     const { income, expense } = eventInfo.event.extendedProps;
 
@@ -46,8 +61,15 @@ function MainCalendar() {
         contentHeight="auto"
         dateClick={handleDateClick} // 날짜 클릭 이벤트
         eventClick={handleEventClick} // 이벤트 클릭 이벤트
-        buttonText={{
-          today: '오늘', // 버튼 텍스트
+        customButtons={{
+          addButton: {
+            text: '+',
+            click: modalOpen,
+          },
+        }}
+        headerToolbar={{
+          left: 'title',
+          right: 'addButton prev,next',
         }}
         events={events.map(event => ({
           title: '',
@@ -56,6 +78,13 @@ function MainCalendar() {
         }))}
         eventContent={renderEventContent} // 커스터마이징된 이벤트 렌더링
       />
+
+      <Button
+        onClick={modalOpen}
+        className="fixed bottom-[calc(90px+env(safe-area-inset-bottom))] right-5 flex h-12 w-12 items-center justify-center rounded-full bg-blue-500 text-2xl text-white shadow-lg"
+      >
+        +
+      </Button>
     </>
   );
 }
