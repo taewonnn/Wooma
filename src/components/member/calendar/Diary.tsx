16b22cadc7e5f33cbf233abd7f03d4e29dc1d1ac
taewonnn <thisis_tw94@naver.com>
2025-01-02 18:24:16 +0900

    feat: calendar - 모달 - form 공통화

--- src/components/member/calendar/Diary.tsx
+++ src/components/member/calendar/Diary.tsx
@@ -1,21 +1,24 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useModalStore } from '../../../stores/useModalStore';
 import Button from '../../common/button/Button';
-import { expenseMethods, incomeMethods } from '../../../constants';
 import { useState } from 'react';
 import Tabs from '../../common/Tabs/Tabs';
 import { Tab } from '@headlessui/react';
+import DiaryForm from './DiaryForm';
 
 // form 데이터 타입 정의
-interface IFormData {
-  category: string;
-  amount: string;
-  detail?: string;
-  method: string;
-  incomeDiary?: string;
-  incomeDiaryImg?: File;
-  expenseDiary?: string;
-  expenseDiaryImg?: File;
+export interface IFormData {
+  category: 'income' | 'expense'; // 탭 구분
+  incomeAmount: string; // 금액(수입)
+  expenseAmount: string; // 금액(지출)
+  incomeDetail?: string; // 상세 내용(수입)
+  expenseDetail?: string; // 상세 내용(지출)
+  incomeMethod: 'salary' | 'allowance' | 'carryOver' | 'other'; // 수단(수입)
+  expenseMethod: 'card' | 'cash' | 'other'; // 수단(지출)
+  incomeDiary?: string; // 다이어리(수입)
+  expenseDiary?: string; // 다이어리(지출)
+  incomeDiaryImg?: File; // 이미지(수입)
+  expenseDiaryImg?: File; // 이미지(지출)
 }
 
 function Diary() {
@@ -43,15 +46,13 @@ function Diary() {
 
     if (file) {
       // console
-      // console.log('file: ', file?.name, URL.createObjectURL(file));
+      console.log('file: ', file?.name, URL.createObjectURL(file));
+      console.log('type: ', type);
 
       setImgFile({ type: type, fileName: file?.name, file: URL.createObjectURL(file) }); // type / 파일명 / URL 저장
       setValue(type, file, { shouldValidate: false }); // hook-form 등록
     }
   };
-
-  // 이미지 미리보기
-  const previewImg = () => {};
 
   // form 제출
   const onSubmit: SubmitHandler<IFormData> = data => {
@@ -66,114 +67,15 @@ function Diary() {
   return (
     <div className="p-5">
       <Tabs tabs={['입금', '지출']} onChange={setSelectedTab}>
-        {/* 입금 탭 */}
+        {/* 수입 탭 */}
         <Tab.Panel className="rounded-xl bg-white p-5 shadow">
           <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
-            {/** 금액 */}
-            <div>
-              <label htmlFor="income" className="text-gray-700 block text-left text-sm font-medium">
-                금액
-              </label>
-              <input
-                id="income"
-                type="text"
-                placeholder="금액"
-                {...register('amount', { required: '금액을 입력하세요.' })}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-            </div>
-
-            {/** 상세내용 */}
-            <div>
-              <label
-                htmlFor="income-detail"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                상세내용
-              </label>
-              <input
-                id="income-detail"
-                type="text"
-                placeholder="메모"
-                {...register('detail')}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-            </div>
-
-            {/** 수단 */}
-            <div className="flex items-center gap-6">
-              {incomeMethods.map(option => (
-                <label key={option.value} className="flex items-center text-left">
-                  <input
-                    type="radio"
-                    value={option.value}
-                    {...register('method')}
-                    className="mr-2"
-                  />
-                  {option.label}
-                </label>
-              ))}
-            </div>
-
-            {/** 다이어리 */}
-            <div>
-              <label
-                htmlFor="incomeDiary"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                다이어리
-              </label>
-              <textarea
-                id="incomeDiary"
-                placeholder="일기"
-                {...register('incomeDiary')}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-            </div>
-
-            {/** 다이어리 - 이미지 */}
-            <div>
-              <label
-                htmlFor="incomeDiaryImg"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                이미지
-              </label>
-
-              <input
-                id="incomeDiaryImg"
-                type="file"
-                style={{ display: 'none' }}
-                {...register('incomeDiaryImg', {
-                  onChange: e => handleImgChange(e, 'incomeDiaryImg'), // 파일 선택 시 처리
-                })}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-              <Button
-                onClick={() => document.getElementById('incomeDiaryImg')?.click()}
-                className="bg-gray-300 text-gray-700 hover:bg-gray-400 rounded-md px-4 py-2 text-sm font-medium shadow"
-              >
-                이미지 추가
-              </Button>
-              <div className="mt-2 h-[20px]">
-                {imgFile?.type === 'incomeDiaryImg' && (
-                  <div className="text-left text-sm underline">
-                    {imgFile.fileName}
-                    {imgFile.file && (
-                      <a
-                        href={imgFile.file}
-                        target="_blank" // 새 창에서 링크를 열기 위한 속성
-                        rel="noopener noreferrer" // 새 창을 열 때 보안 및 성능 향상을 위해 사용하는 속성
-                        className="text-main underline"
-                      >
-                        (이미지 보기)
-                      </a>
-                    )}
-                  </div>
-                )}
-              </div>
-            </div>
-
+            <DiaryForm
+              type="income"
+              register={register}
+              handleImgChange={handleImgChange}
+              imgFile={imgFile}
+            />
             <Button
               type="submit"
               className="rounded-md bg-main px-6 py-2 text-sm font-medium text-white shadow hover:bg-blue-600"
@@ -186,125 +88,22 @@ function Diary() {
         {/* 지출 탭 */}
         <Tab.Panel className="rounded-xl bg-white p-5 shadow">
           <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
-            {/** 금액 */}
-            <div>
-              <label
-                htmlFor="expense"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                금액
-              </label>
-              <input
-                id="expense"
-                type="text"
-                placeholder="금액"
-                {...register('amount', { required: '금액을 입력하세요.' })}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-            </div>
-
-            {/** 상세내용 */}
-            <div>
-              <label
-                htmlFor="expense-detail"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                상세내용
-              </label>
-              <input
-                id="expense-detail"
-                type="text"
-                placeholder="메모"
-                {...register('detail')}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-            </div>
-
-            {/** 결제수단 */}
-            <div className="flex items-center gap-6">
-              {expenseMethods.map(option => (
-                <label key={option.value} className="flex items-center text-left">
-                  <input
-                    type="radio"
-                    value={option.value}
-                    {...register('method')}
-                    className="mr-2"
-                  />
-                  {option.label}
-                </label>
-              ))}
-            </div>
-
-            {/** 다이어리 */}
-            <div>
-              <label
-                htmlFor="expenseDiary"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                다이어리
-              </label>
-              <textarea
-                id="expenseDiary"
-                placeholder="일기"
-                {...register('expenseDiary')}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-            </div>
-
-            {/** 다이어리 - 이미지 */}
-            <div>
-              <label
-                htmlFor="expenseDiaryImg"
-                className="text-gray-700 block text-left text-sm font-medium"
-              >
-                이미지
-              </label>
-
-              <input
-                id="expenseDiaryImg"
-                type="file"
-                style={{ display: 'none' }}
-                {...register('expenseDiaryImg', {
-                  onChange: e => handleImgChange(e, 'expenseDiaryImg'), // 파일 선택 시 처리
-                })}
-                className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-              />
-              <Button
-                type="button"
-                onClick={() => document.getElementById('expenseDiaryImg')?.click()} // 숨겨진 input 클릭
-                className="bg-gray-300 text-gray-700 hover:bg-gray-400 rounded-md px-4 py-2 text-sm font-medium shadow"
-              >
-                이미지 첨부
-              </Button>
-              <div className="mt-2 h-[20px]">
-                {imgFile?.type === 'expenseDiaryImg' && (
-                  <div className="text-left text-sm underline">
-                    {imgFile.fileName}
-                    {imgFile.file && (
-                      <a
-                        href={imgFile.file}
-                        target="_blank" // 새 창에서 링크를 열기 위한 속성
-                        rel="noopener noreferrer" // 새 창을 열 때 보안 및 성능 향상을 위해 사용하는 속성
-                        className="text-main underline"
-                      >
-                        (이미지 보기)
-                      </a>
-                    )}
-                  </div>
-                )}
-              </div>
-            </div>
-
+            <DiaryForm
+              type="expense"
+              register={register}
+              handleImgChange={handleImgChange}
+              imgFile={imgFile}
+            />
             <Button
-              title="확인"
               type="submit"
               className="rounded-md bg-main px-6 py-2 text-sm font-medium text-white shadow hover:bg-blue-600"
-            />
+            >
+              확인
+            </Button>
           </form>
         </Tab.Panel>
       </Tabs>
 
-      {/* 닫기 버튼 */}
       <div className="mt-6 flex justify-end gap-4">
         <Button
           title="닫기"
---
+++ src/components/member/calendar/DiaryForm.tsx
@@ -0,0 +1,137 @@
+import { UseFormRegister } from 'react-hook-form';
+import { expenseMethods, incomeMethods } from '../../../constants';
+import Button from '../../common/button/Button';
+import { IFormData } from './Diary';
+
+interface IDiaryForm {
+  type: 'income' | 'expense';
+  register: UseFormRegister<IFormData>;
+  handleImgChange: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    type: 'incomeDiaryImg' | 'expenseDiaryImg',
+  ) => void;
+  imgFile: { type: string; fileName: string; file: string } | null;
+}
+
+function DiaryForm({ type, register, handleImgChange, imgFile }: IDiaryForm) {
+  const isIncome = type === 'income';
+  const diaryImgType = isIncome ? 'incomeDiaryImg' : 'expenseDiaryImg';
+  const diaryType = isIncome ? 'incomeDiary' : 'expenseDiary';
+  const methods = isIncome ? incomeMethods : expenseMethods;
+
+  return (
+    <>
+      {/** 금액 */}
+      <div>
+        <label
+          htmlFor={`${type}Amount`}
+          className="text-gray-700 block text-left text-sm font-medium"
+        >
+          금액
+        </label>
+        <input
+          id={`${type}Amount`}
+          type="text"
+          placeholder="금액"
+          {...register(`${type}Amount`, { required: '금액을 입력하세요.' })}
+          className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+        />
+      </div>
+
+      {/** 상세내용 */}
+      <div>
+        <label
+          htmlFor={`${type}Detail`}
+          className="text-gray-700 block text-left text-sm font-medium"
+        >
+          상세내용
+        </label>
+        <input
+          id={`${type}Detail`}
+          type="text"
+          placeholder="메모"
+          {...register(`${type}Detail`)}
+          className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+        />
+      </div>
+
+      {/** 수단 */}
+      <div>
+        <label
+          htmlFor={isIncome ? 'incomeMethod' : 'expenseMethod'}
+          className="text-gray-700 mb-2 block text-left text-sm font-medium"
+        >
+          내용
+        </label>
+        <div className="flex items-center gap-4">
+          {methods.map(option => (
+            <label key={option.value} className="flex items-center">
+              <input
+                type="radio"
+                value={option.value}
+                {...register(isIncome ? 'incomeMethod' : 'expenseMethod')}
+                className="mr-2"
+              />
+              {option.label}
+            </label>
+          ))}
+        </div>
+      </div>
+
+      {/** 다이어리 */}
+      <div>
+        <label htmlFor={diaryType} className="text-gray-700 block text-left text-sm font-medium">
+          다이어리
+        </label>
+        <textarea
+          id={diaryType}
+          placeholder="일기"
+          {...register(diaryType)}
+          className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+        />
+      </div>
+
+      {/** 다이어리 - 이미지 */}
+      <div>
+        <label htmlFor={diaryImgType} className="text-gray-700 block text-left text-sm font-medium">
+          이미지
+        </label>
+
+        <input
+          id={diaryImgType}
+          type="file"
+          style={{ display: 'none' }}
+          {...register(diaryImgType, {
+            onChange: e => handleImgChange(e, diaryImgType), // 파일 선택 시 처리
+          })}
+          className="border-gray-300 w-full rounded-md px-3 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+        />
+        <Button
+          onClick={() => document.getElementById(diaryImgType)?.click()}
+          className="bg-gray-300 text-gray-700 hover:bg-gray-400 rounded-md px-4 py-2 text-sm font-medium shadow"
+        >
+          이미지 추가
+        </Button>
+        <div className="mt-2 h-[20px]">
+          {imgFile?.type === diaryImgType && (
+            <div className="text-left text-sm underline">
+              {imgFile.fileName}
+              {imgFile.file && (
+                <a
+                  href={imgFile.file}
+                  target="_blank" // 새 창에서 링크를 열기 위한 속성
+                  rel="noopener noreferrer" // 새 창을 열 때 보안 및 성능 향상을 위해 사용하는 속성
+                  className="text-main underline"
+                >
+                  (이미지 보기)
+                </a>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DiaryForm;
