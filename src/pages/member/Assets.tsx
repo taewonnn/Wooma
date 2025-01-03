48d1358ef73a6c6eea6abd4015d7ab5e91d7f96a
taewonnn <thisis_tw94@naver.com>
2025-01-03 18:09:55 +0900

    feat: asset - 그래프 테스트

--- src/pages/member/Assets.tsx
+++ src/pages/member/Assets.tsx
@@ -1,12 +1,22 @@
 import { ApexOptions } from 'apexcharts';
 import AssetCard from '../../components/member/asset/AssetCard';
 import ReactApexChart from 'react-apexcharts';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
+
+const assets = [
+  { title: '예금', value: '50,000', icon: '' },
+  { title: '적금', value: '10,000', icon: '' },
+  { title: '펀드', value: '100,000,000', icon: '' },
+  { title: '투자', value: '9,555,000', icon: '' },
+  { title: '기타', value: '999,999,999,999', icon: '' },
+];
 
 // 차트의 옵션을 설정하는 객체 - ApexOptions interface를 할당
 const options: ApexOptions = {
   theme: {
-    mode: 'dark',
+    monochrome: { color: 'red' },
   },
+  title: { text: '내 자산', align: 'center', style: { fontSize: '20' } },
   chart: {
     type: 'donut',
     height: 350,
@@ -16,17 +26,18 @@ const options: ApexOptions = {
 const series = [44, 55, 41, 17, 15];
 
 function Assets() {
-  const assets = [
-    { title: '예금', value: '50,000', icon: '' },
-    { title: '적금', value: '10,000', icon: '' },
-    { title: '펀드', value: '100,000,000', icon: '' },
-    { title: '투자', value: '9,555,000', icon: '' },
-    { title: '기타', value: '999,999,999,999', icon: '' },
-  ];
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   return (
     <>
-      <ReactApexChart type="donut" options={options} series={series} />
+      <div className="flex justify-center">
+        <ReactApexChart
+          type="donut"
+          options={options}
+          series={series}
+          width={isMobile ? 400 : 700}
+        />
+      </div>
 
       <div className="mb-20"></div>
 
