1ff33112656d0631f47b3f5a2de88092118ab2a5
taewonnn <thisis_tw94@naver.com>
2025-01-06 10:55:14 +0900

    feat: asset - 그래프 테스트

--- src/pages/member/Assets.tsx
+++ src/pages/member/Assets.tsx
@@ -1,7 +1,6 @@
 import { ApexOptions } from 'apexcharts';
 import AssetCard from '../../components/member/asset/AssetCard';
 import ReactApexChart from 'react-apexcharts';
-import { useMediaQuery } from '../../hooks/useMediaQuery';
 import { assetTypes } from '../../constants';
 import { formatNumber } from '../../constants/common';
 
@@ -14,14 +13,14 @@ const assets = [
   { title: '기타', value: '999999999', icon: '' },
 ];
 
-// 차트의 옵션을 설정하는 객체
-const options: ApexOptions = {
+// 차트 옵션 설정 객체
+const AssetData: ApexOptions = {
+  series: [999999999, 100000000, 9555000, 50000, 10000], // 실 데이터
   title: { text: '내 자산', align: 'center', style: { fontSize: '20' } },
   chart: {
     type: 'donut',
     height: 350,
   },
-  series: [999999999, 100000000, 9555000, 50000, 10000], // 실 데이터
   labels: assetTypes,
   dataLabels: {
     enabled: true,
@@ -29,13 +28,22 @@ const options: ApexOptions = {
       return val + '%';
     },
   },
+  legend: { position: 'bottom' },
+  plotOptions: {
+    pie: {
+      donut: {
+        size: '60%',
+      },
+    },
+  },
+  colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560', '#775DD0'],
 };
 
 function Assets() {
   return (
     <>
       <div className="flex justify-center">
-        <ReactApexChart type="donut" options={options} series={options.series} width={400} />
+        <ReactApexChart type="donut" options={AssetData} series={AssetData.series} width={400} />
       </div>
 
       <div className="mb-20"></div>
