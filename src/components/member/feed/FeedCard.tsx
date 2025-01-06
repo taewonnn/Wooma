45efe29f3679e4c8adfd55aa0182a6b137240a0c
taewonnn <thisis_tw94@naver.com>
2025-01-06 10:54:26 +0900

    feat: feed - 유저 프로필 고정

--- src/components/member/feed/FeedCard.tsx
+++ src/components/member/feed/FeedCard.tsx
@@ -1,4 +1,5 @@
 import { IDiaryData } from '../../../types/diary';
+import Img from '../../common/img/Img';
 
 function FeedCard({ isLoading, diaryList }: any) {
   // console.log('props data : ', diaryList);
@@ -12,17 +13,20 @@ function FeedCard({ isLoading, diaryList }: any) {
             className="card mx-auto mb-8 w-full overflow-hidden rounded-lg bg-white shadow-lg"
             key={diary.id}
           >
+            {/** 타이틀 */}
             <div className="flex items-center space-x-4 p-4">
               <p>{diary.title}</p>
             </div>
 
+            {/** @todo 유저 프로필 */}
             <div className="flex items-center space-x-4 p-4">
-              <img
-                src="/path/to/userImage.jpg"
+              <Img
+                src="/member/profile_example.svg"
                 alt="User"
                 className="h-12 w-12 rounded-full object-cover"
               />
 
+              {/** 이미지 */}
               {diary.imageURL && (
                 <img src={diary.imageURL} alt="" className="h-[300px] w-[300px]" />
               )}
