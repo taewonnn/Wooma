285ea89f74f637a9d2b8d8f586700b105e69188c
taewonnn <thisis_tw94@naver.com>
2025-01-06 12:22:10 +0900

    feat: 공통 - 헤더 탭 상태

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { navMenus } from '../../../constants';
 
 interface INavBarProps {
@@ -7,10 +7,13 @@ interface INavBarProps {
 
 function NavBar({ isMobile = false }: INavBarProps) {
   const navigate = useNavigate();
+  const location = useLocation(); // 현재 URL 정보를 가져옴
 
   const onClickPage = (event: React.MouseEvent<HTMLButtonElement>) => {
     const page = event.currentTarget.textContent?.toLowerCase();
-    navigate(`/${page}`);
+    if (page) {
+      navigate(`/${page}`); // 해당 페이지로 이동
+    }
   };
 
   return (
@@ -19,17 +22,22 @@ function NavBar({ isMobile = false }: INavBarProps) {
         isMobile ? 'flex-row justify-around' : 'flex-col items-start'
       } gap-2 p-2 text-white`}
     >
-      {navMenus.map(navMenu => (
-        <button
-          key={navMenu}
-          onClick={onClickPage}
-          className={`rounded-md px-3 py-2 text-sm hover:bg-purple-700 ${
-            isMobile ? 'flex flex-col items-center' : 'w-full text-left'
-          }`}
-        >
-          {navMenu}
-        </button>
-      ))}
+      {navMenus.map(navMenu => {
+        // 현재 URL과 메뉴 이름을 비교하여 색상을 결정
+        const isActive = location.pathname === `/${navMenu.toLowerCase()}`;
+
+        return (
+          <button
+            key={navMenu}
+            onClick={onClickPage}
+            className={`rounded-md px-3 py-2 text-sm ${
+              isActive ? 'bg-purple-900' : ''
+            } hover:bg-purple-600 ${isMobile ? 'flex flex-col items-center' : 'w-full text-left'}`}
+          >
+            {navMenu}
+          </button>
+        );
+      })}
     </div>
   );
 }
