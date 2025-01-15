07824e3d537ef687c0f6bdc2515b0994464b8259
junnnyyy <junnnyyy@kakao.com>
2025-01-15 22:39:19 +0900

    feature: 코드 다듬기

--- src/pages/member/LocalList.tsx
+++ src/pages/member/LocalList.tsx
@@ -17,8 +17,18 @@ interface ILocalList {
   selectedData?: LocalData[]; // 체크된 지역 코드 리스트
 }
 
-export default function LocalList({ title, type, data, onDetailClick, onCheckChange, selectedData }: ILocalList) {
-  const key= (item: LocalData): string => `${item.depth}#${item.code}`
+export default function LocalList({
+  title,
+  type,
+  data,
+  onDetailClick,
+  onCheckChange,
+  selectedData,
+}: ILocalList) {
+  const key = (item: LocalData): string => `${item.depth}#${item.code}`;
+  const isChecked = (item: LocalData): boolean =>
+    selectedData?.map(data => `${data.depth}#${data.code}`)?.includes(key(item)) || false;
+
   return (
     <dl className="border-e3e text-666 h-[223px] w-[233px] overflow-hidden rounded border bg-white">
       <dt className="boredr-b-e3e bg-f5f border-b p-[10px] text-[12px]">{title}</dt>
@@ -32,11 +42,8 @@ export default function LocalList({ title, type, data, onDetailClick, onCheckCha
                   className={`chkbox_loc_target_plus chk_loc_target_plus_${type} border-e3e checked:!bg-17a flex h-[18px] w-[18px] items-center justify-center rounded-[3px] border bg-white bg-no-repeat checked:bg-[url('./assets/svg/check.svg')] checked:bg-[length:14px_9px] checked:ring-0 focus:shadow-none focus:ring-0`}
                   name={`loc_target_plus_${type}_chk[]`}
                   id={`loc_target_plus_${type}_${item.code}`}
-                  data-code={item.code}
-                  data-depth={type.split('')[0]}
-                  data-name={type === '1st' ? item.name + ' 전체' : item.name}
                   value={item.name}
-                  checked={selectedData?.map(data => `${data.depth}#${data.code}`)?.includes(key(item))}
+                  checked={isChecked(item)}
                   onChange={e => onCheckChange?.(item, e.target.checked)} // 체크박스 상태 전달
                 />
                 <label htmlFor={`loc_target_plus_${type}_${item.code}`}>{item.name}</label>
