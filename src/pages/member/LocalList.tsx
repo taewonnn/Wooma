8b0be7035333bcd1482e542846b495d270fe8f88
TaeWon Park <thisis_tw94@naver.com>
2025-01-15 23:10:32 +0900

    feat: test 중...

--- src/pages/member/LocalList.tsx
+++ src/pages/member/LocalList.tsx
@@ -12,7 +12,7 @@ interface ILocalList {
   type: '1st' | '2nd' | '3rd'; // 타입: 시/도, 구/군, 동/읍/면
   data: LocalData[]; // json 데이터 리스트
   parentCode?: string; // 선택된 상위 지역 코드 (구/군에서 필요)
-  onDetailClick?: (code: string) => void;
+  onDetailClick?: (item: LocalData) => void;
   onCheckChange?: (item: LocalData, isChecked: boolean) => void; // 체크박스 상태 변경 핸들러
 }
 export default function LocalList({ title, type, data, onDetailClick, onCheckChange }: ILocalList) {
@@ -22,34 +22,32 @@ export default function LocalList({ title, type, data, onDetailClick, onCheckCha
       <dd>
         <div className="h-[185px] overflow-y-auto p-[10px]">
           <ul className="flex flex-col gap-[10px]">
-            {data.map(item => (
-              <li key={item.code} className="flex items-center gap-[5px]">
-                <input
-                  type="checkbox"
-                  className={`chkbox_loc_target_plus chk_loc_target_plus_${type} border-e3e checked:!bg-17a flex h-[18px] w-[18px] items-center justify-center rounded-[3px] border bg-white bg-no-repeat checked:bg-[url('./assets/svg/check.svg')] checked:bg-[length:14px_9px] checked:ring-0 focus:shadow-none focus:ring-0`}
-                  name={`loc_target_plus_${type}_chk[]`}
-                  id={`loc_target_plus_${type}_${item.code}`}
-                  data-code={item.code}
-                  data-depth={type.split('')[0]}
-                  data-name={type === '1st' ? item.name + ' 전체' : item.name}
-                  value={item.code}
-                  onChange={e => onCheckChange?.(item, e.target.checked)} // 체크박스 상태 전달
-                />
-                <label htmlFor={`loc_target_plus_${type}_${item.code}`}>{item.name}</label>
-                {(type === '1st' || (type === '2nd' && !item.name.includes('전체'))) && (
-                  <span
-                    id={`loc_target_plus_${type}_${item.code}_span`}
-                    className="flex cursor-pointer"
-                    onClick={() => {
-                      console.log('Detail Click Code: ', item.code);
-                      onDetailClick?.(item.code);
-                    }}
-                  >
-                    &#62; 상세설정
-                  </span>
-                )}
-              </li>
-            ))}
+            {data &&
+              data.map(item => (
+                <li key={item.code} className="flex items-center gap-[5px]">
+                  <input
+                    type="checkbox"
+                    className={`chkbox_loc_target_plus chk_loc_target_plus_${type} border-e3e checked:!bg-17a flex h-[18px] w-[18px] items-center justify-center rounded-[3px] border bg-white bg-no-repeat checked:bg-[url('./assets/svg/check.svg')] checked:bg-[length:14px_9px] checked:ring-0 focus:shadow-none focus:ring-0`}
+                    name={`loc_target_plus_${type}_chk[]`}
+                    id={`loc_target_plus_${type}_${item.code}`}
+                    data-code={item.code}
+                    data-depth={type.split('')[0]}
+                    data-name={type === '1st' ? item.name + ' 전체' : item.name}
+                    value={item.code}
+                    onChange={e => onCheckChange?.(item, e.target.checked)} // 체크박스 상태 전달
+                  />
+                  <label htmlFor={`loc_target_plus_${type}_${item.code}`}>{item.name}</label>
+                  {(type === '1st' || (type === '2nd' && !item.name.includes('전체'))) && (
+                    <span
+                      id={`loc_target_plus_${type}_${item.code}_span`}
+                      className="flex cursor-pointer"
+                      onClick={() => onDetailClick?.(item)}
+                    >
+                      &#62; 상세설정
+                    </span>
+                  )}
+                </li>
+              ))}
           </ul>
         </div>
       </dd>
--- src/pages/member/Test.tsx
+++ src/pages/member/Test.tsx
@@ -4,38 +4,29 @@ import depthData from '../../assets/local_depth.json';
 import { useEffect, useState } from 'react';
 
 function Test() {
-  // 데이터 필터 - 시/도
-  const depth1Items = depthData.filter(data => data.depth === 1);
-
   // 상태관리
   const [selectedList, setSelectedList] = useState<LocalData[]>([]); // 선택한 지역 배열에 추가
-  const [selectedFirstCode, setSelectedFirstCode] = useState<string | null>(null); // 시/도 클릭한 code 저장
-  const [selectedSecondCode, setSelectedSecondCode] = useState<string | null>(null); // 구/군 클릭한 code 저장
+  const [selectedFirstCode, setSelectedFirstCode] = useState<LocalData | null>(null); // 시/도 클릭한 code 저장
+  const [selectedSecondCode, setSelectedSecondCode] = useState<LocalData | null>(null); // 구/군 클릭한 code 저장
 
   /**
    * 상세설정 선택 함수 -  2가지 필요(시/도 - first, 구/군 - second)
    * 다음 뎁스 체크 + 다음 뎁스 노출
    */
-  const handleFirstClick = (code: string) => {
-    setSelectedFirstCode(code);
+  const handleFirstClick = (item: LocalData) => {
+    setSelectedFirstCode(item);
     setSelectedSecondCode(null);
   };
 
-  const handleSecondClick = (code: string) => {
-    setSelectedSecondCode(code);
+  const handleSecondClick = (item: LocalData) => {
+    setSelectedSecondCode(item);
   };
 
-  // 구/군 필터 -> code 이용
-  const filteredSecond = depthData
-    .flatMap(data => data.children)
-    .filter(data => data.parent_code === selectedFirstCode);
-  // console.log('구/군 필터', filteredSecond);
+  // 구/군 필터 data
+  const filteredSecond = selectedFirstCode?.children || [];
 
-  // 동/읍/면 필터 -> code 이용
-  const filteredThird = filteredSecond
-    .filter(data => data.code === selectedSecondCode)
-    .flatMap(data => data.children);
-  // console.log('동/읍/면 필터', filteredThird);
+  // 동/읍/면 필터 data
+  const filteredThird = selectedSecondCode?.children || [];
 
   /**
    * 체크박스 선택 시 함수
@@ -71,7 +62,7 @@ function Test() {
             <td>
               <LocalList
                 title="시/도 (다중선택 가능)"
-                data={depth1Items}
+                data={depthData}
                 type="1st"
                 onDetailClick={handleFirstClick}
                 onCheckChange={handleCheck}
@@ -98,7 +89,7 @@ function Test() {
               <dl className="border-e3e text-666 h-[223px] w-[233px] overflow-hidden rounded border bg-white">
                 <dt className="boredr-b-e3e bg-f5f border-b p-[10px] text-[12px]">선택 지역</dt>
                 <dd className="relative h-[185px] overflow-y-auto p-[10px]">
-                  <div className="w-full"></div>
+                  <div className="w-full">{}</div>
                 </dd>
               </dl>
             </td>
