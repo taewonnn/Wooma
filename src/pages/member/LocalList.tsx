512ed12c6d726b008be1361b5d3ad517f551b434
junnnyyy <junnnyyy@kakao.com>
2025-01-15 21:38:30 +0900

    zzzzz

--- src/pages/member/LocalList.tsx
+++ src/pages/member/LocalList.tsx
@@ -1,7 +1,10 @@
 export interface LocalData {
+  depth: number;
   code: string;
   name: string;
   parent_code?: string;
+  is_whole?: boolean;
+  children?: LocalData[];
 }
 
 interface ILocalList {
@@ -9,10 +12,13 @@ interface ILocalList {
   type: '1st' | '2nd' | '3rd'; // 타입: 시/도, 구/군, 동/읍/면
   data: LocalData[]; // json 데이터 리스트
   parentCode?: string; // 선택된 상위 지역 코드 (구/군에서 필요)
-  onDetailClick?: (code: string) => void;
+  onDetailClick?: (item: LocalData) => void;
   onCheckChange?: (item: LocalData, isChecked: boolean) => void; // 체크박스 상태 변경 핸들러
+  selectedData?: LocalData[]; // 체크된 지역 코드 리스트
 }
-export default function LocalList({ title, type, data, onDetailClick, onCheckChange }: ILocalList) {
+
+export default function LocalList({ title, type, data, onDetailClick, onCheckChange, selectedData }: ILocalList) {
+  const key= (item: LocalData): string => `${item.depth}#${item.code}`
   return (
     <dl className="border-e3e text-666 h-[223px] w-[233px] overflow-hidden rounded border bg-white">
       <dt className="boredr-b-e3e bg-f5f border-b p-[10px] text-[12px]">{title}</dt>
@@ -20,7 +26,7 @@ export default function LocalList({ title, type, data, onDetailClick, onCheckCha
         <div className="h-[185px] overflow-y-auto p-[10px]">
           <ul className="flex flex-col gap-[10px]">
             {data.map(item => (
-              <li key={item.code} className="flex items-center gap-[5px]">
+              <li key={key(item)} className="flex items-center gap-[5px]">
                 <input
                   type="checkbox"
                   className={`chkbox_loc_target_plus chk_loc_target_plus_${type} border-e3e checked:!bg-17a flex h-[18px] w-[18px] items-center justify-center rounded-[3px] border bg-white bg-no-repeat checked:bg-[url('./assets/svg/check.svg')] checked:bg-[length:14px_9px] checked:ring-0 focus:shadow-none focus:ring-0`}
@@ -29,7 +35,8 @@ export default function LocalList({ title, type, data, onDetailClick, onCheckCha
                   data-code={item.code}
                   data-depth={type.split('')[0]}
                   data-name={type === '1st' ? item.name + ' 전체' : item.name}
-                  value={item.code}
+                  value={item.name}
+                  checked={selectedData?.map(data => `${data.depth}#${data.code}`)?.includes(key(item))}
                   onChange={e => onCheckChange?.(item, e.target.checked)} // 체크박스 상태 전달
                 />
                 <label htmlFor={`loc_target_plus_${type}_${item.code}`}>{item.name}</label>
@@ -39,7 +46,7 @@ export default function LocalList({ title, type, data, onDetailClick, onCheckCha
                     className="flex cursor-pointer"
                     onClick={() => {
                       console.log('Detail Click Code: ', item.code);
-                      onDetailClick?.(item.code);
+                      onDetailClick?.(item);
                     }}
                   >
                     &#62; 상세설정
--- src/pages/member/Test.tsx
+++ src/pages/member/Test.tsx
@@ -1,127 +1,190 @@
 import LocalList, { LocalData } from './\bLocalList';
-import firstLocal from '../../assets/first_local.json';
-import secondLocal from '../../assets/second_local.json';
-import thirdLocal from '../../assets/third_local.json';
+import wholeData from '../../assets/local_depth.json';
 import { useEffect, useState } from 'react';
 
 function Test() {
-  // 체크박스 리스트 상태관리
+
+  // const wholeData = localData;
+
+  /**
+   * 화면에서 실제로 눈에 보이는 데이터, 시/도 or 구/군 체크박스 변동시 변경됨
+   */
+  const [districtSectionData, setDistrictSectionData] = useState<LocalData[]>([]);
+  const [townSectionData, setTownSectionData] = useState<LocalData[]>([]);
+
+  /**
+   * 화면노출을위해 최근에 선택한 데이터 저장
+   */
+  const [recentSelectedCity, setRecentSelectedCity] = useState<string | null>(null);
+  const [recentSelectedDistrict, setRecentSelectedDistrict] = useState<string | null>(null);
+
+
+  /**
+   * 실제로 선택된 지역 리스트
+   */
   const [selectedData, setSelectedData] = useState<LocalData[]>([]);
-  // 현재 선택된 시/도 코드를 저장
-  const [selectedCityCode, setSelectedCityCode] = useState<string | null>(null);
-  // 현재 선택된 구/군 코드를 저장
-  const [selectedDistrictCode, setSelectedDistrictCode] = useState<string | null>(null);
 
-  // 시/도 상세 설정 클릭 시 동작
-  const handleCityDetailClick = (code: string) => {
-    setSelectedCityCode(code);
-    setSelectedDistrictCode(null); // 구/군 초기화
+  // 시/도 상세 설정 클릭 시 동작 없으면 채우고 있으면 비우고
+  const handleCityDetailClick = (item: LocalData) => {
+    const code = item.code;
+    const city = wholeData.find(data => data.code === code);
+    console.log({ 'city': city });
+    if (recentSelectedCity === code) {
+      setDistrictSectionData([]);
+      setRecentSelectedCity(null);
+    } else {
+      setDistrictSectionData(city.children);
+      setRecentSelectedCity(code);
+    }
   };
 
-  // 구/군 상세 설정 클릭 시 동작
-  const handleDistrictDetailClick = (code: string) => {
-    setSelectedDistrictCode(code);
+  // 구/군 상세 설정 클릭 시 동작  없으면 채우고 있으면 비우고
+  const handleDistrictDetailClick = (item: LocalData) => {
+    const code = item.code;
+    const district = districtSectionData.find(data => data.code === code);
+    console.log({ 'district': district });
+    if (recentSelectedDistrict === code) {
+      setTownSectionData([]);
+      setRecentSelectedDistrict(null);
+    } else {
+      setTownSectionData(district.children);
+      setRecentSelectedDistrict(code);
+    }
   };
 
-  // 지역 선택 시 데이터 담기
+  function hasChild(item) {
+    return item?.depth === 1 || item?.depth === 2;
+  }
+
+  /**
+   * @description 지역 선택 시 데이터 담기
+   * @param item 선택한 아이템
+   * @param isChecked 체크여부
+   */
   const handleCheckChange = (item: LocalData, isChecked: boolean) => {
+
+    console.log('item', item);
+    console.log('isChecked', isChecked);
+    /*체크하면 selectedDate에 item 추가*/
     if (isChecked) {
+      /* 1, 2 번 뎁스면 첫번째 자식까지만 추가*/
+      if (hasChild(item)) {
+        if (item?.children) {
+          setSelectedData(prev => [...prev, item?.children[0]]);
+        }
+      }
+      /* 나와 같은 뎁스, 같은부모에 선택된 is_whole이 있으면 제외 */
+      const isWhole = selectedData.some(data => data.depth === item.depth && data.parent_code === item.parent_code && data.is_whole);
+      if (selectedData.some(data => isWhole)) {
+        setSelectedData(prev => prev.filter(data => !isWhole || !data.is_whole));
+      }
       setSelectedData(prev => [...prev, item]);
     } else {
-      setSelectedData(prev => prev.filter(data => data.code !== item.code));
+      /**
+       * 체크 해제하면 selectedDate 에서 item 제거
+       * selectedData에서 제거된 item의 code가 parent_code인 orphan 데이터가 있는지 확인하고 selectedData에서 orphan데이터도 제거
+       * */
+      setSelectedData(prev => prev.filter(data => data !== item && data.parent_code !== item.code));
+
     }
+    console.log({ selectedData });
   };
 
-  // 삭제
-  const handleRemoveSelected = (code: string) => {
-    setSelectedData(prev => prev.filter(data => data.code !== code));
-  };
+  /**
+   * 자식에의해 보여지는 데이터인가?
+   * check는 되어있고, 노출은 안되어야하는 케이스를 위해 존재하는 함수
+   * */
+  const isRepresentativeByChild = (item): boolean => {
+    return hasChild(item) && selectedData && selectedData.some(data => data.depth === item.depth + 1 && data.parent_code === item.code && data.is_whole);
+  }
 
-  // 선택 지역 리스트 - 상위 지역 정보 찾기
+  /*부모 가져오기*/
+  const getParent = (item: LocalData): LocalData | undefined => {
+    return selectedData && selectedData.find(data => data.depth === item.depth - 1 && data.code === item.parent_code);
+  }
+
+// 선택 지역 리스트 - 상위 지역 정보 찾기
   const getFullLocationName = (item: LocalData): string => {
-    const district = secondLocal.find(d => d.code === item.parent_code);
-    const city = firstLocal.find(c => c.code === district?.parent_code);
-
-    if (item.code.length === 2) {
-      return `${item.name} 전체`;
-    } else {
-      return `(${city?.name || ''}) ${district?.name || ''} ${item.name}`;
+    console.log({ selectedData });
+    console.log({ item });
+    console.log({"Dd": isRepresentativeByChild(item)})
+    const district = item.depth === 3 ? getParent(item)?.name : '';
+    const city = item.depth === 3? getParent(getParent(item)!!)?.name : getParent(item)?.name;
+    switch (item.depth) {
+      case 1:
+        return isRepresentativeByChild(item) ? '' : `${item.children[0].name} 1`;
+      case 2:
+        return isRepresentativeByChild(item) ? '' : item.is_whole ? item.name : `(${city}) ${item.name} 2`;
+      case 3:
+        return item.is_whole ? `(${city}) ${item.name} 3` : `(${city}) ${district} ${item.name} 3`;
+      default:
+        return;
     }
   };
-
-  // 선택된 시/도에 해당하는 구/군 데이터 필터링
-  const filteredDistricts = secondLocal.filter(
-    district => district.parent_code === selectedCityCode,
-  );
-
-  useEffect(() => {
-    console.log('districts', filteredDistricts);
-  }, [setSelectedDistrictCode]);
-
-  // 선택된 구/군에 해당하는 동/읍/면 데이터 필터링
-  const filteredTowns = thirdLocal.filter(town => town.parent_code === selectedDistrictCode);
-  console.log(filteredTowns);
 
   return (
     <div className="flex flex-wrap gap-[20px]">
       <table>
         <tbody>
-          {/** @todo 장소 설정 리스트 */}
-          <tr className="flex flex-wrap gap-[20px]">
-            <td>
-              <LocalList
-                title="시/도 (다중선택 가능)"
-                data={firstLocal}
-                type="1st"
-                onDetailClick={handleCityDetailClick}
-                onCheckChange={handleCheckChange} // 함수 전달
-              />
-            </td>
-            <td>
-              <LocalList
-                title="구/군 (다중선택 가능)"
-                data={filteredDistricts}
-                type="2nd"
-                onDetailClick={handleDistrictDetailClick}
-                onCheckChange={handleCheckChange} // 함수 전달
-              />
-            </td>
-            <td>
-              <LocalList
-                title="동/읍/면 (다중선택 가능)"
-                data={filteredTowns}
-                type="3rd"
-                onCheckChange={handleCheckChange} // 함수 전달
-              />
-            </td>
-            <td>
-              <dl className="border-e3e text-666 h-[223px] w-[233px] overflow-hidden rounded border bg-white">
-                <dt className="boredr-b-e3e bg-f5f border-b p-[10px] text-[12px]">선택 지역</dt>
-                <dd className="relative h-[185px] overflow-y-auto p-[10px]">
-                  <div className="w-full">
-                    {selectedData.length === 0 ? (
-                      <div
-                        id="loc_target_plus_result_none"
-                        className="loc_target_plus_result_area absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-[10px] whitespace-nowrap"
-                      >
-                        {/* 기본 문구 */}
-                        선택한 시/군/구가 노출됩니다.
-                      </div>
-                    ) : (
-                      <ul id="loc_target_plus_result">
-                        {selectedData.map(item => (
-                          <li key={item.code} className="flex justify-between">
-                            <span>{getFullLocationName(item)}</span>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </div>
-                </dd>
-              </dl>
-            </td>
-          </tr>
-          <tr></tr>
+        {/** @todo 장소 설정 리스트 */}
+        <tr className="flex flex-wrap gap-[20px]">
+          <td>
+            <LocalList
+              title="시/도 (다중선택 가능)"
+              data={wholeData}
+              type="1st"
+              onDetailClick={handleCityDetailClick}
+              onCheckChange={handleCheckChange} // 함수 전달
+              selectedData={selectedData}
+            />
+          </td>
+          <td>
+            <LocalList
+              title="구/군 (다중선택 가능)"
+              data={districtSectionData}
+              type="2nd"
+              onDetailClick={handleDistrictDetailClick}
+              onCheckChange={handleCheckChange} // 함수 전달
+              selectedData={selectedData}
+            />
+          </td>
+          <td>
+            <LocalList
+              title="동/읍/면 (다중선택 가능)"
+              data={townSectionData}
+              type="3rd"
+              onCheckChange={handleCheckChange} // 함수 전달
+              selectedData={selectedData}
+            />
+          </td>
+          <td>
+            <dl className="border-e3e text-666 h-[223px] w-[233px] overflow-hidden rounded border bg-white">
+              <dt className="boredr-b-e3e bg-f5f border-b p-[10px] text-[12px]">선택 지역</dt>
+              <dd className="relative h-[185px] overflow-y-auto p-[10px]">
+                <div className="w-full">
+                  {selectedData.length === 0 ? (
+                    <div
+                      id="loc_target_plus_result_none"
+                      className="loc_target_plus_result_area absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-[10px] whitespace-nowrap"
+                    >
+                      {/* 기본 문구 */}
+                      선택한 시/군/구가 노출됩니다.
+                    </div>
+                  ) : (
+                    <ul id="loc_target_plus_result">
+                      {selectedData.map(item => (
+                        <li key={`${item.depth}-${item.code}`} className="flex justify-between">
+                          <span>{getFullLocationName(item)}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              </dd>
+            </dl>
+          </td>
+        </tr>
+        <tr></tr>
         </tbody>
       </table>
     </div>
